caae5ef2bb95701371feea5469619cd08fe1371a
Danya <danylo.yakovenko@gmail.com>
2022-02-17 20:38:24 +0200

    start

--- main/src/main.java
+++ main/src/main.java
@@ -1,7 +1,15 @@
-
+import java.util.Scanner;
+import java.util.Random;
 
 public class main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        String[] qe = new String[]{"q", "w", "e"};
+        for (int i = 0; qe.length < i; i++) {
+            System.out.println(qe);
+        }
+        //while (scan.hasNextInt()) {
+        //}
     }
 }
